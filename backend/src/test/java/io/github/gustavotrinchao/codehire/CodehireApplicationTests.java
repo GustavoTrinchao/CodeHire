f02043b9bf53bfdb808b92d26bad081777268066
Gustavo Trinchão <gustavotrinchao@ufba.br>
2026-07-18 18:34:11 -0300

    chore: create initial project structure

---
+++ backend/src/test/java/io/github/gustavotrinchao/codehire/CodehireApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gustavotrinchao.codehire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodehireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
