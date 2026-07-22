6d6925df00f8ef98293a76819a0de9b31530a0df
Gustavo Trinchão <gustavotrinchao@ufba.br>
2026-07-22 12:51:08 -0300

    feat: create register page

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
-import LoginPage from "./pages/login/LoginPage.tsx";
 import HomePage from "./pages/home/HomePage.tsx";
+import LoginPage from "./pages/login/LoginPage.tsx";
+import RegisterPage from "./pages/register/RegisterPage.tsx";
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
       <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   </BrowserRouter>
 );
---
+++ frontend/src/pages/register/RegisterPage.tsx
@@ -0,0 +1,69 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Link } from "react-router-dom";
+import logo from "@/assets/logo.svg";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+function RegisterPage() {
+  return (
+    <div>
+      <main className="min-h-screen bg-blue flex flex-col gap-8 items-center justify-center bg-slate-50">
+        <header className="flex items-center gap-3">
+          <img src={logo} alt="CodeHire logo" className="h-8 w-8" />
+          <h1 className="text-xl font-semibold">CodeHire</h1>
+        </header>
+        <div className="flex flex-col gap-5 w-full max-w-sm bg-white rounded-xl shadow-lg p-6">
+          <div>
+            <h2 className="text-xl font-medium">Create your account</h2>
+          </div>
+          <form className="flex flex-col gap-4">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="name">Full name</label>
+              <Input className="h-9" id="name" placeholder="jonh doe"/>
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email">Email address</label>
+              <Input className="h-9" id="email" placeholder="jonhdoe@company.com"/>
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="password" >Password</label>
+              <Input className="h-9" id="password" type="password" placeholder="••••••••"/>
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email">Company (optional)</label>
+              <Input className="h-9" id="company" placeholder="TechCorp Inc."/>
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="role">I am a...</label>
+              <Select>
+                <SelectTrigger className="w-full h-9">
+                  <SelectValue placeholder="Select a role" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="recruiter">Recruiter / Hiring Manager</SelectItem>
+                  <SelectItem value="devoloper">Devoloper/ Candidate</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex flex-col items-center">
+              <Button type="submit" className="h-9 w-full bg-blue-600 hover:bg-blue-700">Create account</Button>
+            </div>
+          </form>
+        </div>
+        <footer className="flex flex-col items-center gap-3">
+          <p>Already have an account? <Link to="/login" className="text-blue-600 font-semibold">Sign in</Link></p>
+          <Link to="/" className="font-semibold text-slate-400">← Back to homepage</Link>
+        </footer>
+      </main>
+    </div>
+
+  );
+}
+
+export default RegisterPage;
